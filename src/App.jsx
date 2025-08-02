4390e942562dd331444ecd32222f359b4f76a6cb
RRRishav <rishav6787@gmail.com>
2025-08-02 12:56:54 +0530

    setup webcam in react adding tensorflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from 'react'
+import * as tf from "@tensorflow/tfjs"
+import * as facemesh from "@tensorflow-models/facemesh"
+import Webcam from "react-webcam"
 
 function App() {
-  const [count, setCount] = useState(0)
+     
+  const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <> 
+       <div className="app">
+        <Webcam ref={webcamRef} style={
+            {
+              position:"absolute",
+              marginLeft:"auto",
+              marginRight:"auto",
+              left:0,
+              right:0,
+              textAlign:"center",
+              zIndex:9,
+              width:640,
+              height:480
+            }
+        }></Webcam>
+       </div>
     </>
   )
 }
